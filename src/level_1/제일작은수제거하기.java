34c18fbf26d8dd977840a933a8535697cee1e684
Gwak seyeon <ksyk1205@naver.com>
2021-03-20 20:10:34 +0900

    day06

---
+++ src/level_1/제일작은수제거하기.java
@@ -0,0 +1,85 @@
+package level_1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 제일작은수제거하기 {
+
+	public static void main(String[] args) {
+		/*
+		문제 설명
+		정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요.
+		단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, 
+		[10]면 [-1]을 리턴 합니다.
+
+		제한 조건
+		arr은 길이 1 이상인 배열입니다.
+		인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+		입출력 예
+		arr	return
+		[4,3,2,1]	[4,3,2]
+		[10]	[-1]
+		*/
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("배열의 요소 수?");
+		int num = stdIn.nextInt();
+		
+		int[] arr = new int[num];
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print("arr["+i+"]");
+			arr[i] = stdIn.nextInt();
+		}
+		int[] solution  = solution(arr);
+		
+		Arrays.toString(solution);
+		
+		
+	}
+	public static int[] solution(int[] arr) {
+        /*
+		if(arr.length<=1) {
+        	return new int[]{-1};
+        }
+        List<Integer> array = new ArrayList<Integer>();
+        
+        for(int i=0; i<arr.length; i++) {
+        	array.add(arr[i]);
+        }
+        int minidx = 0;
+        for(int i=1; i<arr.length; i++) {
+        	if(arr[minidx]>arr[i]) {
+        		minidx = i;
+        	}
+        }
+        array.remove(minidx);
+        int[] answer = new int[array.size()];
+        
+        for(int i=0; i<answer.length; i++) {
+        	answer[i] = array.get(i);
+        }
+        */	
+		if(arr.length<=1) {
+           return new int[]{-1};
+        } 
+        int min = arr[0];
+        for(int i=1; i<arr.length; i++) {
+           if(min>arr[i]) {
+              min = arr[i];
+           }
+        }
+        int[] answer = new int[arr.length-1];
+        int j =0;
+        for(int i=0;i<arr.length;++i) {
+           if(arr[i] == min) {
+              continue;
+           }
+           answer[j] = arr[i];
+           j++;
+        }
+        
+        return answer;
+    }
+
+}
