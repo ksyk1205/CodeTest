967b6d510718beeb889eb8e9d3855b4333084587
Gwak seyeon <ksyk1205@naver.com>
2021-06-17 12:33:51 +0900

    programmers_level1

--- programers/level_1/문자열내p와y의개수.java
+++ programers/level_1/문자열내p와y의개수.java
@@ -7,7 +7,10 @@ public class 문자열내p와y의개수 {
 	public static void main(String[] args) {
 		/*
 		문제 설명
-		대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+		대문자와 소문자가 섞여있는 문자열 s가 주어집니다. 
+		s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 
+		'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 
+		단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
 
 		예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
 
@@ -37,16 +40,15 @@ public class 문자열내p와y의개수 {
         boolean answer = true;
         int count = 0 ;
         for(int i=0; i<s.length(); i++) {
-        	if('p'==s.charAt(i) || 'P'==s.charAt(i)) {
+        	if(s.charAt(i) == 'P' || s.charAt(i) == 'p') {
         		count++;
-        	}else if('y'==s.charAt(i) || 'Y'==s.charAt(i)) {
+        	}else if(s.charAt(i) == 'Y' || s.charAt(i) == 'y'){
         		count--;
         	}
         }
         if(count != 0) {
         	answer = false;
         }
-        
         return answer;
 	}
 
