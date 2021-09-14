9c917dc4bfb514264a774a0eccd8c562452a6970
Gwak seyeon <ksyk1205@naver.com>
2021-09-14 17:51:04 +0900

    개

---
+++ src/baekJoon/realization/Dog.java
@@ -0,0 +1,16 @@
+package baekJoon.realization;
+
+/*개를 출력한다.*/
+
+public class Dog {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+
+	}
+
+}
