2539187ba83b5e528bdfca410646dccb269b8f73
Gwak seyeon <ksyk1205@naver.com>
2021-09-17 22:04:37 +0900

    팩토리얼

--- src/baekJoon/realization/Factorial.java
+++ src/baekJoon/realization/Factorial.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*0보다 크거나 같은 정수 N이 주어진다. 
+ * 이때, N!을 출력하는 프로그램을 작성하시오.*/
+
 public class Factorial {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
