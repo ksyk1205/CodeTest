4aa3051d16b83d8fdccb4c77fcb0b1e3881f06ab
Gwak seyeon <ksyk1205@naver.com>
2021-10-16 12:03:08 +0900

    제로

---
+++ src/baekJoon/stack/Zero.java
@@ -0,0 +1,36 @@
+package baekJoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Zero {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int K = Integer.parseInt(br.readLine());
+		
+		Stack<Integer> stack = new Stack<>();
+		for(int i=0; i<K; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num==0) {
+				stack.pop();
+			}else {
+				stack.push(num);
+			}
+		}
+		int length = stack.size();
+		int sum = 0;
+		if(!stack.empty()) {
+			for(int i=0; i<length; i++) {
+				sum += stack.pop();
+			}
+		}
+		
+		System.out.println(sum);
+	}
+
+}
