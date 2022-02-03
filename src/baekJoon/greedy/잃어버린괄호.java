854b22bda38410b66692313f0262150affd1b620
Gwak seyeon <ksyk1205@naver.com>
2022-02-03 22:39:19 +0900

    잃어버린 괄호

--- src/baekJoon/greedy/잃어버린괄호.java
+++ src/baekJoon/greedy/잃어버린괄호.java
@@ -4,32 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class LostParenthesis {
-
+public class ÀÒ¾î¹ö¸°°ýÈ£ {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		String str = br.readLine();
 		
 		String[] arr = str.split("-");
-		int sum = 0; 
+		int sum = 0;
 		for(int i=0; i<arr.length; i++) {
 			int plus = 0;
-			String[] s = arr[i].split("\\+");
-				
-			for(int j=0; j<s.length; j++) {
-				plus += Integer.parseInt(s[j]);
+			if(arr[i].contains("+")) {
+				String[] s1 = arr[i].split("\\+");
+				for(int j=0; j<s1.length; j++) {
+					plus += Integer.parseInt(s1[j]);
+				}
+			}else {
+				plus =Integer.parseInt(arr[i]);
 			}
 			
-			
 			if(i==0) {
-				sum+=plus;
+				sum +=plus;
 			}else {
-				sum-=plus;
+				sum -=plus;
 			}
 		}
 		System.out.println(sum);
-		
 	}
-
 }
