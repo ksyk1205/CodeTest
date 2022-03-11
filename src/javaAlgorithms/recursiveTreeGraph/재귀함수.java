f4b4627e526fa66c1f4076155ac859e1638c6841
Gwak seyeon <ksyk1205@naver.com>
2022-03-11 21:05:08 +0900

    01.재귀 함수(스택 프레임)

---
+++ src/javaAlgorithms/recursiveTreeGraph/재귀함수.java
@@ -0,0 +1,23 @@
+package javaAlgorithms.recursiveTreeGraph;
+
+import java.util.Scanner;
+
+public class Àç±ÍÇÔ¼ö {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		permulate(N);
+		
+	}
+
+	private static void permulate(int n) {
+		if(n==0) {
+			return;
+		}else {
+			permulate(n-1);
+			System.out.print(n+" ");
+		}
+		
+	}
+}
